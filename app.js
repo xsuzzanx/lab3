5c99f4ba6b9edd65e247829f943c7d9d007561f0
xsuzzanx <zuza.gry@gmail.com>
2024-03-25 18:57:40 +0100

    zadanie 1

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app= express();
+const PORT = 3000;
+
+//home endpoint
+app.get('/home', (req,res)=>{
+    res.title = "HOME";
+    res.body = 'HOME PAGE';
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>${res.title}</title>
+        </head>
+        <body>
+            <p>${res.body}</p>
+        </body> 
+    </html>      
+    `);
+});
+
+//student endpoint
+app.get('/student', (req,res)=>{
+    res.title = "STUDENT";
+    res.body = 'STUDENT PAGE';
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>${res.title}</title>
+        </head>
+        <body>
+            <p>${res.body}</p>
+        </body> 
+    </html>      
+    `);
+});
+
+// add-student endpoint
+app.get('/add-student', (req,res)=>{
+    res.title = "ADD-STUDENT";
+    res.body = 'ADD-STUDENT PAGE';
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>${res.title}</title>
+        </head>
+        <body>
+            <p>${res.body}</p>
+        </body> 
+    </html>      
+    `);
+});
+
+app.listen(PORT,()=>{
+    console.log('Setver listening on port 3000');
+});
